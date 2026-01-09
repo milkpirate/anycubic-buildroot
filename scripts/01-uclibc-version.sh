0e81fc30f1a0d5be048757551f4ea5ef8b1bcc96
Paul Schroeder <milkpirate@users.noreply.github.com>
2026-01-09 03:46:17 +0100

    fix(build): missing curl, use wget instead, mack script fail early
    Signed-off-by: Paul Schroeder <milkpirate@users.noreply.github.com>

--- scripts/01-uclibc-version.sh
+++ scripts/01-uclibc-version.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
 # "Fuzzy" patch to get the desired uclibc version, after that the (real) patches and configs work fine.
-
 UCLIBC_MK="buildroot/package/uclibc/uclibc.mk"
 UCLIBC_HASH="buildroot/package/uclibc/uclibc.hash"
 TARGET_VER="1.0.31"
 
+set -e  # Exit on any error
+
 if [[ ! -f "$UCLIBC_MK" ]]; then
-  echo "  [uClibc] Error: $UCLIBC_MK not found."
+  echo "  [uClibc] Error: $UCLIBC_MK not found." >&2
   exit 1
 fi
 
@@ -17,13 +17,18 @@ if ! grep -q "UCLIBC_VERSION = $TARGET_VER" "$UCLIBC_MK"; then
 fi
 
 if [[ ! -f "$UCLIBC_HASH" ]]; then
-  echo "  [uClibc] Error: $UCLIBC_HASH not found."
+  echo "  [uClibc] Error: $UCLIBC_HASH not found." >&2
   exit 1
 fi
 
-SHA256=$(curl --silent --connect-timeout 5 "https://downloads.uclibc-ng.org/releases/$TARGET_VER/uClibc-ng-$TARGET_VER.tar.xz.sha256" | cut -f1 -d' ')
+SHA_URL="https://downloads.uclibc-ng.org/releases/$TARGET_VER/uClibc-ng-$TARGET_VER.tar.xz.sha256"
+echo "  [uClibc] Downloading SHA256 from $SHA_URL"
+FILE_SHA=
+FILE_SHA="$(wget --timeout=5 --tries=3 -q -O- "$SHA_URL")"
 
-if ! grep -q "$SHA256" "$UCLIBC_HASH"; then
-  echo "sha256  $SHA256  uClibc-ng-$TARGET_VER.tar.xz" >> "$UCLIBC_HASH"
+if ! grep -q "$FILE_SHA$" "$UCLIBC_HASH"; then
+  echo "sha256  $FILE_SHA" >> "$UCLIBC_HASH"
   echo "  [uClibc] Added SHA256 hash for version $TARGET_VER"
+else
+  echo "  [uClibc] SHA256 already present"
 fi
